6b4ff89864ccac69789c27677ca58edb22961579
Saurabh Deshpande <saurabhdeshpande93@gmail.com>
2020-07-20 17:59:06 +0200

    Errors fixed for force array size in for loop

--- Canti.py
+++ Canti.py
@@ -1,14 +1,19 @@
 import Sofa
 import numpy as np
+import sys 
+import time 
+
+t1= time.time()
 
 class CantileverBeam(Sofa.PythonScriptController):
 
 
-	def __init__(self, node, node_name, force,store):
+	def __init__(self, node, node_name, force,store,n_total):
 		self.createGraph(node, node_name, force)
 		self.node_name = node_name
 		self.store = store
-		self.force = force  
+		self.force = force 
+		self.n_total = n_total 
 	
 
 	def createGraph(self, rootNode, node_name,force):
@@ -34,26 +39,25 @@ class CantileverBeam(Sofa.PythonScriptController):
 		self.store.append(self.dofs.position)
 		self.store = np.array(self.store)
 
-		n_total = len(self.force)
-		print(n_total)
-		
-		if self.node_name == n_total -1:
+		if self.node_name == self.n_total -1:
 			print('And the stored values are ')
 			#print(self.store)
-			print(self.store)
-			self.store = self.store - self.store[0]
-			print(self.store)
+			#print(self.store)
+			self.store = self.store - self.store[0] #works only when intitial entry is zero
+			#print(self.store)
 
-			self.store = self.store.reshape(n_total*12096,1)
+			self.store = self.store.reshape(self.n_total*12096,1)
 			f_value = np.zeros((self.store.shape[0],self.store.shape[1]))
 			
-			f = np.linspace(0,10,3) #same as the input force 
+			f = np.linspace(0,10,1000) #same as the input force 
 
-			for i in range(n_total):
+			for i in range(self.n_total):
 				f_value[12096*i] = f[i]
 
-			# data=np.hstack((f_value,self.store))
-			# np.savetxt("SOFA_data.csv", data, delimiter=",")
+			data=np.hstack((f_value,self.store))
+			np.savetxt("SOFA_data_test_500.csv", data, delimiter=",")
+
+			sys.exit()
 
 
 
@@ -67,13 +71,20 @@ def createScene(rootNode):
 	rootNode.createObject('StaticSolver',name='static_beam', newton_iterations=10)
 	#rootNode.createObject('EulerImplicitSolver',name='cg_odesolver',printLog='false')
 	rootNode.createObject('CGLinearSolver',name='linear solver',iterations=100,tolerance=1.0e-9,threshold=1.0e-9)
-	force = np.linspace(0,10,3) #same above
+	force = np.linspace(0,10,1000) #same above
+	n_total = len(force)
 	#force = [-5,5,5]
 	store = []
 
+	t1= time.time()
+
 	for i in range(len(force)):
 		print('Position when force value is ' + str(i))
-		controller = CantileverBeam(rootNode, i, [0,force[i],0],store)
+		controller = CantileverBeam(rootNode, i, [0,force[i],0],store,n_total)
+
+	t2 = time.time()
+	
+	print("Run time is", t2-t1)	
 
 
 		# controller.ff.force = [0,i,0]
